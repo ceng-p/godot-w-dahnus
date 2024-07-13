48e7c03a0abdb0e48b059ad6ddb450a823555f93
debraxy <dennisbraschi.junkmail@gmail.com>
2024-07-13 18:57:53 +0200

    sky box

--- scripts/snowman.gd
+++ scripts/snowman.gd
@@ -10,7 +10,6 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var progress_bar = $ProgressBar
 
-
 @onready var timer = $Timer
 var health = 100
 func _physics_process(delta):
@@ -18,10 +17,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += (gravity / 2) * delta
 	
-	if velocity.x > 0:
-		velocity.x =- SPEED
-	elif velocity.x < 0:
-		velocity.x += SPEED
+		velocity.x =0
 		
 	# Handle jump.
 	if timer.is_stopped() and is_on_floor():
