fa83b457e05e65495e544d5c887dfa9d5db1df02
98christianp <christianpetersen98@gmail.com>
2024-07-13 17:50:26 +0200

    Snowman yo

--- scripts/snowman.gd
+++ scripts/snowman.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +14,15 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	var timer = Timer.new()
+	timer.set_one_shot(false)
+	timer.set_autostart(true)
+	timer.start(5)
+	if timer.is_stopped() and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 
 	move_and_slide()
